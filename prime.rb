4ca2c8373f78cd09c0de5203f3a427be79d643b6
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-13 18:37:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+      is_prime = false
+
+  if num < 0 || num == 1
+    is_prime = false
+  elsif num > 0 && (num.is_a? Integer) 
+    
+    array = (1..num ).to_a 
+      array_divisible = []
+
+      array.each { |el| array_divisible << el if  num % el == 0}
+       
+    array_divisible.length > 2 ? is_prime = false : is_prime = true
+
+  end
+  is_prime
+end
