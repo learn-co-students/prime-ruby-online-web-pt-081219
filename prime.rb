d305cb36932c3253a0b0ef58f20ecbccf47378bf
James Scalise <jamescscalise@gmail.com>
2019-08-19 15:10:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return FALSE
+  else
+    i = 2
+    while i < number
+      if number%i == 0
+        return FALSE
+      end
+      i +=1
+    end
+  end
+  return TRUE
+end
