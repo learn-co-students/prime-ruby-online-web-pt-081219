cfe38ebbe0a870eb01c829375ed6c37e312b47d6
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-20 17:20:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1 
+    return false 
+  elsif number == 3 || number == 2 
+    return true
+  elsif number.even?
+    return false 
+  else 
+    (4..(number - 1)).none? do |num|
+      number % num == 0 
+      end 
+  end 
+end 
+
