c61eba81f46253615473108d5255b602e7ead884
arnie b <arnold.bwaila@gmail.com>
2019-08-24 21:36:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(integer)
+  cnt = 2 
+  if integer > 1 
+    lastnumber = Integer.sqrt(integer).to_i
+  else return false 
+  end
+  range = (2..lastnumber).to_a
+  
+  range.all? do |diviser|
+    integer%diviser != 0
+  end
+end
+    
