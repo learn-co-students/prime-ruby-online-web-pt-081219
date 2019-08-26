67a0a19928749c4c7c724c5e6f89606d7a4660e6
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-26 00:12:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(int)
+  if (int <= 3)
+    return (int > 1)
+  elsif ((int % 2 == 0) || (int % 3 == 0))
+    return false
+  else
+    i = 5
+    while ((i * i) <= int)
+      if ((int % i == 0) || (int / (i + 2) == 0))
+        return false
+      end
+      i += 1
+    end
+    return true
+  end
+end
