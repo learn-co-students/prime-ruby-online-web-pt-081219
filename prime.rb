0436db8e953c36934575f38b887b4633376f0614
Jeffrey Brown <brownjs03@gmail.com>
2019-08-28 01:43:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  test = num - 1 
+  array = (1..test).to_a
+  array.each do |val|
+    if num % val == 0 
+      false 
+    else
+      true 
+    end
+  end
+  
